7a92b9d7a0b19e2ed495cfdc3fe7556f60135a19
Alysson Luiz <105462928+alyss0nluiz@users.noreply.github.com>
2023-02-09 19:57:46 -0300

    Create Dados.js

---
+++ react/atividade3.1/src/components/Dados.js
@@ -0,0 +1,11 @@
+function Dados (props) {
+    return (
+        <section>
+            <p>Nome: {props.nome}</p>
+            <p>Idade: {props.idade}</p>
+            <p>cargo: {props.cargo}</p>
+        </section>
+    )
+}
+
+export default Dados;
